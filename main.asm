f5cf042b93a55c2f39d4c46eac7a0d6caf1ee64c
slplsswkds <slplsswkds@protonmail.com>
2024-08-04 18:25:03 +0300

    Args reading

--- main.asm
+++ main.asm
@@ -2,6 +2,25 @@ global _start
 
 section .text
 _start: ;------------------------------------------
+    ; Read MAC from args
+    ; [rsp] = argc
+    ; [rsp + 8] = argv[]
+    cmp qword [rsp], 1
+    jbe exit_error  ; exit if argc <= 1. (Arguments missing)
+
+    ; reading argv[0]
+    mov rbx, [rsp + 8]  ; 
+    xor rcx, rcx
+count_chars:
+    xor rax, rax
+    mov al, [rbx + rcx]
+    cmp byte al, 0
+    je eol  ; jump if EOL
+    inc rcx
+    jmp count_chars
+
+eol:
+
     ; Create socket
     mov rax, 41     ; socket syscall
     mov rdi, 2      ; family - AF_INET
@@ -15,32 +34,32 @@ _start: ;------------------------------------------
 
     ; Set up sockaddr_in structure
     mov word [sockaddr_in], 2               ; sin_family = AF_INET
-    mov word [sockaddr_in + 2], 0010h       ; sin_port
+    mov word [sockaddr_in + 2], 0900h       ; sin_port (0010h = 4096)
     mov dword [sockaddr_in + 4], 0x0100007F ; in_addr
     mov qword [sockaddr_in + 8], 0x00       ; sin_zero
 
     ; send message
     mov rax, 44             ; sendto syscall
     mov rdi, [socket_fd]    ; socket descriptor
-    mov rsi, msg            ; pointer to the message
-    mov rdx, 12             ; message len
+    mov rsi, magic_packet   ; pointer to the message
+    mov rdx, 102            ; message len
     xor r10, r10            ; flags = NULL 
     mov r8, sockaddr_in     ; *dest_addr 
     mov r9, 16              ; sockaddr len
     syscall
-; exit_success:
+
+exit_success:
     mov rdi, 0
     mov rax, 60
     syscall
 
 exit_error:
     mov rdi, 1  ; general error
-    mov rax, 1  ; exit
+    mov rax, 60 ; exit
     syscall
 ;--------------------------------------------------
 
-section .data ;------------------------------------
-    msg db "UDP test...", 0
+;section .data ;------------------------------------
 ;--------------------------------------------------
 
 section .rodata ;----------------------------------
